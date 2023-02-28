b0245e971278228a59e63c746ea87990a7468d18
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2023-02-28 15:48:45 -0300

    delete instagram-galery

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import LandingPage from "./components/LandingPage";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { InstagramGallery } from "instagram-gallery";
+
 function App() {
   const accesTokenIG = "24c5849f572ff7c2957e560f5f3215eb";
   return (
     <div className="App">
       <LandingPage />
-      <InstagramGallery accessToken={accesTokenIG} count={24} />
     </div>
   );
 }
