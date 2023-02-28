73de870048e07d912f228befafe950fef56e75cc
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2023-02-28 15:51:29 -0300

    delete const acces content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  const accesTokenIG = "24c5849f572ff7c2957e560f5f3215eb";
   return (
     <div className="App">
       <LandingPage />
